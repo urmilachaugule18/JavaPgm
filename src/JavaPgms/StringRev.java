a848737cc57c0201ae400397e8770a676d8a37e1
aksha <urmilachaugule18@gmail.com>
2023-08-07 11:06:19 +0530

    java pgm

---
+++ src/JavaPgms/StringRev.java
@@ -0,0 +1,12 @@
+package JavaPgms;
+
+public class StringRev {
+	
+	public static void main(String[] args) {
+		String a="Cucumber";
+		for(int i=a.length()-1;i>=0;i--) {
+			System.out.print(a.charAt(i));
+		}
+	}
+
+}
